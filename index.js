263eea033bfb5b6ec2fe3bdf40e4f68368c4c47d
Fred Hajdarpasic <ferhat.hajdarpasic@aon.com>
2018-12-19 16:21:49 +1100

    Added winston logging.

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-var http=require('http');
-    http.createServer(function (req,res) {
-        res.writeHead(200, {'Content-type':'text/plain'});
-        res.end('Hello World');
-    }).listen(8080);
+const express = require('express');  
+const bodyParser = require('body-parser');  
+const url = require('url');  
+const querystring = require('querystring');
+
+let winston = require('winston');
+
+let logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(info => {
+            return `${info.timestamp} ${info.level}: ${info.message}`;
+        })
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({filename: 'test.log'})
+    ]
+});
+
+let app = express();  
+app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(bodyParser.json());
+
+// Function to handle the root path
+app.get('/', async function(req, res) {
+    logger.log('info', JSON.stringify(req.query));
+    res.writeHead(200, {'Content-type':'text/plain'});
+    res.end('Hello World');
+});
+
+let server = app.listen(8080, function() {  
+    logger.log('info', 'Server is listening on port 8080')
+});
